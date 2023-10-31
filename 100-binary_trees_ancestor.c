915d22a04073c9d4b1ae659854fa441128bc9d30
Sarahgad <sarahgad91@gmail.com>
2023-10-31 20:22:02 +0200

    agin

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,10 +1,12 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_inorder - a function that goes through
- * a binary tree using in-order traversal
+ * binary_tree_preorder - a function that goes through
+ * a binary tree using pre-order traversal
  * @tree:  a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node
  */
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
    if (!first || !second)
