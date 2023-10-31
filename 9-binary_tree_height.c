7e253278e61ab07ff6af67aab54f3239321c55ae
Sarahgad <sarahgad91@gmail.com>
2023-10-31 17:20:04 +0200

    try

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (-1);
-	if (tree && !tree->left && !tree->right)
+	if (!tree->left && !tree->right)
 		return (0);
 
 	l_height = binary_tree_height(tree->left);
