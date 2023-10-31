e2d9dd8002a9634ef47d3b9163ff56ff67568e27
Sarahgad <sarahgad91@gmail.com>
2023-10-31 21:20:49 +0200

    do you work

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,24 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - a function that goes through
- * a binary tree using in-order traversal
- * @tree:  a pointer to the root node of the tree to traverse
- * @func: a pointer to a function to call for each node
+ * binary_trees_ancestor - Find the lowest common ancestor
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
+ * Return: Pointer to the lowest common ancestor node, or NULL if none found
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
-   if (!first || !second)
-		return NULL;
-	if (first == second)
-		return (binary_tree_t *)second;
-	if (first == second->parent) 
-		return (binary_tree_t *)first;
-	if (first->parent == second)
-		return first->parent;
-	if(first->parent == second->parent)
-		return first->parent;
-	
-    return (binary_tree_t *)binary_trees_ancestor((binary_tree_t *)first->parent,(binary_tree_t *)second->parent);
+	binary_tree_t *ancestor;
+
+	if (!first || !second)
+		return (NULL);
+	if (first == second || (first->parent == second))
+		return ((binary_tree_t *)first->parent);
+	if (first == second->parent)
+		return ((binary_tree_t *)first);
+	if (first->parent == second->parent)
+		return (first->parent);
+
+	ancestor = (binary_tree_t *)binary_trees_ancestor(
+	(binary_tree_t *)first->parent,
+	(binary_tree_t *)second->parent);
+	if (!ancestor && first->parent)
+	return (first->parent);
+	if (!ancestor && second->parent)
+	return (second->parent);
+return (ancestor);
 }
