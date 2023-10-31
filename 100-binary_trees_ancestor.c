24047926c27198b02852cc43da166e14199f0402
Sarahgad <sarahgad91@gmail.com>
2023-10-31 20:42:31 +0200

    try 100

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - a function that goes through
- * a binary tree using pre-order traversal
+ * binary_tree_inorder - a function that goes through
+ * a binary tree using in-order traversal
  * @tree:  a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node
  */
@@ -11,9 +11,10 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 {
    if (!first || !second)
 		return NULL;
-    if (first->parent == second->parent)
-        return (binary_tree_t *)first->parent;
-    if (first == second->parent)
-        return (binary_tree_t *)first;
-	return binary_trees_ancestor(first->parent,second->parent);
+
+	binary_trees_ancestor((binary_tree_t *)first->parent,(binary_tree_t *)second->parent);
+    return (binary_tree_t *)first;
+    binary_trees_ancestor(second->left, second->right);
+    return (binary_tree_t *)second->parent;
+
 }
