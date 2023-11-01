eca4d9d26cb4da1927b346689087e46bd1e49b79
Sarahgad <sarahgad91@gmail.com>
2023-11-01 11:27:37 +0200

    check 101 again

--- binary_trees.h
+++ binary_trees.h
@@ -50,7 +50,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_print(const binary_tree_t *);
-size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_heigh(const binary_tree_t *tree);
 void levelorde_display(const binary_tree_t *tree,  void (*func)(int), size_t height);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 #endif
