01a962fd24f3f526148c99fd1d43a0414dbecd8c
ManarElsaid <elsaidmanar@gmail.com>
2023-10-31 18:12:37 -0700

    18 - uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,6 +13,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (node->parent == node->parent->parent->right)
 		return (node->parent->parent->left);
-	else	
+
+	if (node->parent == node->parent->parent->left)
 		return (node->parent->parent->right);
+	else
+		return (NULL);
 }
