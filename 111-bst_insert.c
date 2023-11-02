5175173e5ee261488c6d448eb8e329b88335af10
Sarahgad <sarahgad91@gmail.com>
2023-11-02 03:34:56 +0200

    logic 111

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -18,21 +18,21 @@ bst_t *bst_insert(bst_t **tree, int value)
 
 	if (value < (*tree)->n)
 	{
-		if ((*tree)->left)
+		if (!(*tree)->left)
 		{
-			return (bst_insert(&((*tree)->left), value));
-		}
 			(*tree)->left = binary_tree_node(*tree, value);
 			return ((*tree)->left);
-
+		}
+		return (bst_insert(&((*tree)->left), value));
 	}
 	else if (value > (*tree)->n)
 	{
-		if ((*tree)->right)
-			return (bst_insert(&((*tree)->right), value));
-
+		if (!(*tree)->right)
+	{
 		(*tree)->right = binary_tree_node(*tree, value);
-			return ((*tree)->right);
+		return ((*tree)->right);
+	}
+		return (bst_insert(&((*tree)->right), value));
 	}
 	return (*tree);
 }
