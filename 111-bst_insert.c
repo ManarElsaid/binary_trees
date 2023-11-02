78830a561658c846954292779f14b1069da4ddef
Sarahgad <sarahgad91@gmail.com>
2023-11-02 02:26:51 +0200

    check 111

---
+++ 111-bst_insert.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+bst_t *bst_insert(bst_t **tree, int value)
+{ 
+    if(tree)
+    {
+    if (*tree == NULL)
+    return(binary_tree_node(*tree, value));
+    if(value < (*tree)->n)
+    *tree = bst_insert(&((*tree)->left), value);
+    else if (value > (*tree)->n)
+    *tree = bst_insert(&((*tree)->right), value);
+return(*tree);
+}
+        return NULL;
+}
