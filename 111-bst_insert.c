225f5a39ad87cd26f9b61599c0361eb5ddacd48c
Sarahgad <sarahgad91@gmail.com>
2023-11-02 03:31:34 +0200

    try

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -19,8 +19,9 @@ bst_t *bst_insert(bst_t **tree, int value)
 	if (value < (*tree)->n)
 	{
 		if ((*tree)->left)
+		{
 			return (bst_insert(&((*tree)->left), value));
-
+		}
 			(*tree)->left = binary_tree_node(*tree, value);
 			return ((*tree)->left);
 
